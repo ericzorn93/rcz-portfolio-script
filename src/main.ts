4758911e2004ff929feef16b6ec71820b62a535d
Eric Zorn <zornwebdev@gmail.com>
2021-07-31 13:20:38 -0400

    fix: tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { CustomCEFHttpService } from "./utils/http/custom_cef-http-service";
 
 async function main(): Promise<void> {
-  await CustomCEFHttpService.getCefCustomDailyPrices(5000);
-  // console.log(symbols);
+  const dailyPrices = await CustomCEFHttpService.getCefCustomDailyPrices();
+  console.log(dailyPrices);
 }
 
 main();
